39613c1a1276f2b1bade86e05eeb5a7b6f19fb4e
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-01-08 18:41:34 -0800

    Updated main.py
    Added calculation to count votes per candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,4 @@
-# Python analysis of csv file with 3 columns (election_data.csv)
+# Python analysis of election csv file with 3 columns
 
 # columns are Voter ID, County, and Candidate
 
@@ -9,6 +9,9 @@ import csv
 # initial variables
 vote_count = 0
 candidates = []
+vote_candidates = {}
+winner = ""
+winner_count = 0
 
 # set filepath to csv
 csvpath = os.path.join(".", "Resources", "election_data.csv")
@@ -28,17 +31,19 @@ with open(csvpath) as csvfile:
     # iterate through rows
     for row in csvreader:
     
-        # Total mnumber of votes cast (count of voter ID)
+        # Total number of votes cast (count of rows)
         vote_count += 1
 
-        # complete list of candidates who received votes (count unique candidates)
-        if row[2] not in candidates:
-            candidates.append(row[2])
-
-        # Percentage of votes each candidate won
-
-
+        # complete list of candidates who received votes
         # total number of votes each candidate won
+        if row[2] not in candidates:
+            candidates.append(row[2]) # append unique candidates to list
+            vote_candidates[row[2]] = 1  # add candidate to dictionary as key with value 1
+        else:
+            vote_candidates[row[2]] +=1  # increase value in dictionary by 1
+            
+            
+        # Percentage of votes each candidate won
 
 
         # winner of election based on popular vote
@@ -60,3 +65,7 @@ print("----------------------------")
 
 
 # export results to text file
+
+
+
+
